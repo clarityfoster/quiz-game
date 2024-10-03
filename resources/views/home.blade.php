468712a7420323e2f05b247734cb955bf52fe38e
clarity552 <cherrymyintmo552@gmail.com>
2024-10-03 23:30:49 +0630

    add responsive for home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="custom-container">
-        <div class="d-flex align-items-center justify-content-between">
-            <div class="align-items-start"  style="max-width: 700px">
-                <h2 class="mb-4 title-line-height">
+        <div class="d-flex align-items-center justify-content-between home-content">
+            <div class="home-contents"  style="max-width: 700px">
+                <h2 class="mb-4 title-line-height home-title">
                     Welcome to the Ultimate <span class="blue">Quiz Challenge!</span>
                 </h2>
                 <p class="home-para">
-                    Ready to put your knowledge to the test? Our quizzes are designed to entertain and challenge players of all ages.Are you up for the challenge? Let the fun begin! 
+                    Ready to put your knowledge to the test? Our quizzes are designed to entertain and challenge players of all ages. Are you up for the challenge? Let the fun begin! 
                 </p>
-                <a href="#" class="btn btn-warning btn-lg text-white mt-3">Get Started</a>
+                <a href="#" class="btn btn-warning btn-lg text-white mt-3 get-started-btn">Get Started</a>
             </div>
-            <img src="{{ asset('img/home/home.png') }}" alt="">
+            <img class="home-img" src="{{ asset('img/home/home.png') }}" alt="">
         </div>
     </div>
 @endsection
